d3c24bfcec89625d097bab17d78495e5b8eb1aef
TR-SLimey <tr_slimey@protonmail.com>
2020-05-14 21:56:31 +0100

    Small code style consistency edits

--- SERVER/API/helpers/misc.php
+++ SERVER/API/helpers/misc.php
@@ -2,24 +2,35 @@
 
 // Get IP of client
 function getClientIP() {
+
     $IPAddress = 'UNKNOWN';
     $keys = array('HTTP_CLIENT_IP','HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_FORWARDED_FOR','HTTP_FORWARDED','REMOTE_ADDR');
-    foreach($keys as $k)
-    {
-        if (isset($_SERVER[$k]) && !empty($_SERVER[$k]) && filter_var($_SERVER[$k], FILTER_VALIDATE_IP))
-        {
+    foreach($keys as $k) {
+
+        if (isset($_SERVER[$k]) && !empty($_SERVER[$k]) && filter_var($_SERVER[$k], FILTER_VALIDATE_IP)) {
+
             $IPAddress = $_SERVER[$k];
             break;
+
         }
+
     }
+
     return $IPAddress;
+
 }
 
 // Check if an array has all of the keys
 function hasKeys($array, $keys) {
+
     if (!(count(array_diff($keys, array_keys($array))) === 0)) {
+
         return false;
+
     } else {
+
         return true;
+
     }
+
 }
