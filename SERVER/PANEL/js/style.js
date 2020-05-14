c9481a74ac69627917f227a281d9933b27a465ec
TR-SLimey <tr_slimey@protonmail.com>
2020-05-14 22:11:40 +0100

    Removed lingering parts of animsition

--- SERVER/PANEL/js/style.js
+++ SERVER/PANEL/js/style.js
@@ -84,28 +84,6 @@
 
 (function ($) {
     "use strict";
-    $(".animsition").animsition({
-      inClass: 'fade-in',
-      outClass: 'fade-out',
-      inDuration: 600,
-      outDuration: 600,
-      linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([class^="chosen-single"])',
-      loading: true,
-      loadingParentElement: 'html',
-      loadingClass: 'page-loader',
-      loadingInner: '<div class="page-loader__spin"></div>',
-      timeout: false,
-      timeoutCountdown: 5000,
-      onLoadEvent: true,
-      browser: ['animation-duration', '-webkit-animation-duration'],
-      overlay: false,
-      overlayClass: 'animsition-overlay-slide',
-      overlayParentElement: 'html',
-      transition: function (url) {
-        window.location.href = url;
-      }
-    });
-
 
   })(jQuery);
 (function ($) {
