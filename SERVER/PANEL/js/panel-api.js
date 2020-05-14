f3a561f31990fe45a208086479bad660567d5bfe
TR-SLimey <tr_slimey@protonmail.com>
2020-05-14 23:44:51 +0100

    Started cleanup of panel API function

--- SERVER/PANEL/js/panel-api.js
+++ SERVER/PANEL/js/panel-api.js
@@ -19,67 +19,89 @@ function api(data) {
     fullPrefix += ["2", "4", "6", "8"][Math.floor(Math.random() * 4)]; // Add panel ID char
     fullPrefix += protocolVersion;
 
+    // Generate the second layer
     // JSON encode the data to be sent
-    data = JSON.stringify(data);
+    var finalData = JSON.stringify(data);
     // and encrypt it with the session token (second layer)
-    var finalData = aes.encrypt(data, sessionToken);
+    finalData = aes.encrypt(finalData, sessionToken);
 
     // Generate the first layer
-    var firstLayerPayload = [
+    var finalData = [
         sessionID,
         finalData,
     ];
     // JSON encode the first layer
-    firstLayerPayload = JSON.stringify(firstLayerPayload);
+    finalData = JSON.stringify(finalData);
     // Encrypt the first layer with the first layer key and add the prefix
-    const finalPayload = fullPrefix + aes.encrypt(firstLayerPayload, firstLayerEncryptionKey);
-    console.log(finalPayload);
+    const finalData = fullPrefix + aes.encrypt(finalData, firstLayerEncryptionKey);
 
     // Define what happens when a response from the API is received
     var finalResponse;
     request.onreadystatechange = function() {
+
         // If the response was received successfully
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+
             // The response could be encrypted but it might not be
             try {
-                // Try decrypting the response
-                response = aes.decrypt(request.responseText, cryptKey);
+
+                // Try decrypting the response (API responses are only encrypted)
+                // with the session token
+                response = aes.decrypt(request.responseText, sessionToken);
                 // Parse the JSON response
                 finalResponse = JSON.parse(response);
                 // Verify the fingerprint of the server (only if the communication is encrypted because otherwise the server does not attach it)
                 if (finalResponse["APIFingerprint"] != trustedAPIFingerprint) {
-                    throw new Error("the API did not provide a trusted fingerprint (expected = `" + response_dict["server_id"] + "` | received = `" + trusted_server_signature + "`");
+
+                    console.log("WARNING: the API did not provide a trusted fingerprint (expected = `" + response_dict["server_id"] + "` | received = `" + trusted_server_signature + "`. The API response will be ignored.");
+                    return;
+
                 }
+
             } catch (err) {
+
                 // If we got here, the response was not encrypted or the server provided the wrong ID
                 // If the latter, re-throw the error so the user can see
                 if (err.message.startsWith("The server provided an incorrect ID. ")) { throw err; }
                 // If we got here, that means it was just a simple decryption problem so get the response as plaintext
                 response = request.responseText;
                 // Parse the JSON response
-                try {response_dict = JSON.parse(response);}
+                try {
+
+                    response_dict = JSON.parse(response);
+
                 // If the API response is not valid JSON, log the error
-                catch {
+                } catch {
+
                     console.log("The API response contains invalid JSON");
                     // Set both response variables to undefined so this error can be detected down the line
                     response = undefined;
                     response_dict = undefined;
+
                 }
+
             }
+
             // If the API returns a message, tell the user
             if ("message" in response_dict) {
+
                 console.log("The server says: "+response_dict["message"]);
+
             }
+
         // If the API returns an error in the form of a HTTP code
         } else if (this.readyState == 4 && this.status != 200) {
+
             console.log("the API returned a non-200 code when called (" + this.status + ")");
             // Set both response variables to undefined so this error can be detected down the line
             response = undefined;
             response_dict = undefined;
+
         }
+
     }
 
+    // Send the API request
     request.open("POST", APILocation, true);
     request.send(finalPayload);
 
