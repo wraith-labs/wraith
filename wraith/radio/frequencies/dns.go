670031a37ba6bf5c6e597fa414a21a14711b76f6
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-22 14:51:29 +0000

    Created HTTP "frequency"

--- wraith/radio/frequencies/dns.go
+++ wraith/radio/frequencies/dns.go
@@ -1,4 +1,11 @@
 package frequencies
 
-type DNS struct {
+type DNS struct{}
+
+func (freq DNS) Transmit(url string, data []byte) error {
+	return nil
+}
+
+func (freq DNS) Receive(url string) ([]byte, error) {
+	return []byte{}, nil
 }
--- wraith/radio/frequencies/http.go
+++ wraith/radio/frequencies/http.go
@@ -1,4 +1,32 @@
 package frequencies
 
-type HTTP struct {
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+type HTTP struct{}
+
+func (freq HTTP) Transmit(url string, data []byte) error {
+	resp, err := http.Post(url, "application/octet-stream", bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// Don't bother reading the response, there's not a lot we can do about errors anyway
+	return nil
+}
+
+func (freq HTTP) Receive(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
