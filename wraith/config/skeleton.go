8b987c1366e8c5e862e641aa8724c464567512d8
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-22 12:38:45 +0000

    Added more settings to the config skeleton

--- wraith/config/skeleton.go
+++ wraith/config/skeleton.go
@@ -3,7 +3,22 @@ package config
 type ConfigSkeleton struct {
 	Radio struct {
 		Transmitter struct {
-			DefaultURLGenerator string
+			DefaultURLGenerator    string
+			DefaultIntervalSeconds int
+			Encryption             struct {
+				Enabled bool
+				Type    int
+				Key     string
+			}
+		}
+		Receiver struct {
+			DefaultURLGenerator    string
+			DefaultIntervalSeconds int
+			Encryption             struct {
+				Enabled bool
+				Type    int
+				Key     string
+			}
 		}
 	}
 }
