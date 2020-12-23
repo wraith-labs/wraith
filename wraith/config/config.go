2fc4bfa5f61d4241d141197d3bbfd6ed3ab8f47f
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-23 16:30:59 +0000

    Set some temporary defaults in the config

--- wraith/config/config.go
+++ wraith/config/config.go
@@ -3,5 +3,15 @@ package config
 var Config ConfigSkeleton
 
 func Init() {
-	Config.Radio.Transmitter.DefaultURLGenerator = "package gen"
+	Config.Radio.Transmitter.DefaultURLGenerator = "package gen\nfunc Gen(old string) string {\nreturn 'http://localhost:8080'\n}"
+	Config.Radio.Transmitter.DefaultIntervalSeconds = 2
+	Config.Radio.Transmitter.Encryption.Enabled = false
+	Config.Radio.Transmitter.Encryption.Type = 0
+	Config.Radio.Transmitter.Encryption.Key = ""
+
+	Config.Radio.Receiver.DefaultURLGenerator = "package gen\nfunc Gen(old string) string {\nreturn 'http://localhost:8080'\n}"
+	Config.Radio.Receiver.DefaultIntervalSeconds = 2
+	Config.Radio.Receiver.Encryption.Enabled = false
+	Config.Radio.Receiver.Encryption.Type = 0
+	Config.Radio.Receiver.Encryption.Key = ""
 }
