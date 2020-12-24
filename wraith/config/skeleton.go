aa235f7768b00c19e9d24f2d18520462588d23d8
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-24 13:02:42 +0000

    Switched from an interval to a trigger function... for transmit and receive. This is superior as it allows more flexibility as the trigger function can trigger at specified times, or in certain conditions which makes for a much less detectable and predictable program.

--- wraith/config/skeleton.go
+++ wraith/config/skeleton.go
@@ -1,22 +1,20 @@
 package config
 
-import "time"
-
 type ConfigSkeleton struct {
 	Radio struct {
 		Transmitter struct {
-			DefaultURLGenerator    string
-			DefaultIntervalSeconds time.Duration
-			Encryption             struct {
+			DefaultURLGenerator string
+			DefaultTrigger      string
+			Encryption          struct {
 				Enabled bool
 				Type    int
 				Key     string
 			}
 		}
 		Receiver struct {
-			DefaultURLGenerator    string
-			DefaultIntervalSeconds time.Duration
-			Encryption             struct {
+			DefaultURLGenerator string
+			DefaultTrigger      string
+			Encryption          struct {
 				Enabled bool
 				Type    int
 				Key     string
