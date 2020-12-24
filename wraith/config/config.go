66272a9bf0639233288d6fc77307020efb770505
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-24 13:09:28 +0000

    Added option to configure the interval at which... the comms trigger function executes

--- wraith/config/config.go
+++ wraith/config/config.go
@@ -4,14 +4,17 @@ var Config ConfigSkeleton
 
 func Init() {
 	Config.Radio.Transmitter.DefaultURLGenerator = "package gen\nfunc Gen(old string) string {\nreturn 'http://localhost:8080'\n}"
-	Config.Radio.Transmitter.DefaultIntervalSeconds = 2
+	Config.Radio.Transmitter.DefaultTrigger = "package trigger\nfunc Trigger(curTimestamp int) bool {\nif curTimestamp % 5 == 0 {\nreturn true\n} else {\nreturn false\n}\n}"
+	Config.Radio.Transmitter.DefaultTriggerCheckInterval = 1
 	Config.Radio.Transmitter.Encryption.Enabled = false
 	Config.Radio.Transmitter.Encryption.Type = 0
 	Config.Radio.Transmitter.Encryption.Key = ""
 
 	Config.Radio.Receiver.DefaultURLGenerator = "package gen\nfunc Gen(old string) string {\nreturn 'http://localhost:8080'\n}"
-	Config.Radio.Receiver.DefaultIntervalSeconds = 2
+	Config.Radio.Receiver.DefaultTrigger = "package trigger\nfunc Trigger(curTimestamp int) bool {\nif curTimestamp % 6 == 0 {\nreturn true\n} else {\nreturn false\n}\n}"
+	Config.Radio.Receiver.DefaultTriggerCheckInterval = 1
 	Config.Radio.Receiver.Encryption.Enabled = false
 	Config.Radio.Receiver.Encryption.Type = 0
 	Config.Radio.Receiver.Encryption.Key = ""
+
 }
--- wraith/config/skeleton.go
+++ wraith/config/skeleton.go
@@ -3,18 +3,20 @@ package config
 type ConfigSkeleton struct {
 	Radio struct {
 		Transmitter struct {
-			DefaultURLGenerator string
-			DefaultTrigger      string
-			Encryption          struct {
+			DefaultURLGenerator         string
+			DefaultTrigger              string
+			DefaultTriggerCheckInterval int
+			Encryption                  struct {
 				Enabled bool
 				Type    int
 				Key     string
 			}
 		}
 		Receiver struct {
-			DefaultURLGenerator string
-			DefaultTrigger      string
-			Encryption          struct {
+			DefaultURLGenerator         string
+			DefaultTrigger              string
+			DefaultTriggerCheckInterval int
+			Encryption                  struct {
 				Enabled bool
 				Type    int
 				Key     string
