b1f040023b1a2b58f497c6702ba802ce4087325d
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2021-03-30 20:23:45 +0100

    Config comment

--- wraith/config/config.go
+++ wraith/config/config.go
@@ -3,6 +3,9 @@ package config
 var Config ConfigSkeleton
 
 func init() {
+	// Whether Wraith should exit upon receiving a signal from the system instructing it to do so. Enabling this makes the Wraith less
+	// likely to be caught or deemed as suspicious by the user, while disabling makes Wraith more resilient. Note that some signals
+	// cannot be intercepted and will therefore always kill Wraith regardless of this setting.
 	Config.Process.RespectExitSignals = true
 
 	Config.Comms.Transmitter.URLGenerator = "package gen\nfunc Gen(old string) string {\nreturn \"http://localhost:8080\"\n}"
