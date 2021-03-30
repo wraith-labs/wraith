d165d800938f260c11246f764c9f9fbd4db74e59
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2021-03-30 20:02:21 +0100

    Made hooks thread-safe

--- wraith/hooks/hooks.go
+++ wraith/hooks/hooks.go
@@ -1,32 +1,83 @@
 package hooks
 
+import "sync"
+
 // Hook types
 type onStartHook func()
 type onCommandHook func(string) string
 type onExitHook func()
 
+// Hook list types
+type onStartHookList struct {
+	data  []onStartHook
+	mutex sync.Mutex
+}
+
+func (l *onStartHookList) Add(hook onStartHook) {
+	defer l.mutex.Unlock()
+	l.mutex.Lock()
+	l.data = append(l.data, hook)
+}
+func (l *onStartHookList) Range(f func(hook onStartHook)) {
+	for _, hook := range l.data {
+		f(hook)
+	}
+}
+
+type onCommandHookList struct {
+	data  []onCommandHook
+	mutex sync.Mutex
+}
+
+func (l *onCommandHookList) Add(hook onCommandHook) {
+	defer l.mutex.Unlock()
+	l.mutex.Lock()
+	l.data = append(l.data, hook)
+}
+func (l *onCommandHookList) Range(f func(hook onCommandHook)) {
+	for _, hook := range l.data {
+		f(hook)
+	}
+}
+
+type onExitHookList struct {
+	data  []onExitHook
+	mutex sync.Mutex
+}
+
+func (l *onExitHookList) Add(hook onExitHook) {
+	defer l.mutex.Unlock()
+	l.mutex.Lock()
+	l.data = append(l.data, hook)
+}
+func (l *onExitHookList) Range(f func(hook onExitHook)) {
+	for _, hook := range l.data {
+		f(hook)
+	}
+}
+
 // Hook lists
-var OnStart []onStartHook
-var OnCommand []onCommandHook
-var OnExit []onExitHook
+var OnStart onStartHookList
+var OnCommand onCommandHookList
+var OnExit onExitHookList
 
 // Hook runners
 func RunOnStart() {
-	for _, hook := range OnStart {
+	OnStart.Range(func(hook onStartHook) {
 		hook()
-	}
+	})
 }
 
 func RunOnCommand(command string) []string {
 	results := []string{}
-	for _, hook := range OnCommand {
+	OnCommand.Range(func(hook onCommandHook) {
 		results = append(results, hook(command))
-	}
+	})
 	return results
 }
 
 func RunOnExit() {
-	for _, hook := range OnExit {
+	OnExit.Range(func(hook onExitHook) {
 		hook()
-	}
+	})
 }
