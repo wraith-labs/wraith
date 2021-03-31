fa1dc1dd096ed43b53676cdcdd70f24d769042b2
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2021-04-01 00:29:32 +0100

    Implement basic debug Rx

--- wraith/comms/channels/rx/debug.go
+++ wraith/comms/channels/rx/debug.go
@@ -7,12 +7,38 @@ When included, it "receives" a print command every 2 seconds.
 
 package rx
 
-import "github.com/0x1a8510f2/wraith/comms"
+import (
+	"time"
+
+	"github.com/0x1a8510f2/wraith/comms"
+)
 
 var Debug comms.Rx
 
 func init() {
-	Debug.Start = func() { println(1) }
-	Debug.Stop = func() {}
+	// Register handler for the debug:// URL scheme (which is never really used)
 	comms.RegRx("debug", &Debug)
+	// Create a channel to trigger exit via the `Stop` method
+	Debug.Data["exitTrigger"] = make(chan struct{})
+	// On start, run a thread pushing a debug message every 2 seconds
+	Debug.Start = func() {
+		go func() {
+			defer close(Debug.Data["exitTrigger"].(chan struct{}))
+			for {
+				select {
+				case <-Debug.Data["exitTrigger"].(chan struct{}):
+					return
+				case <-time.After(2 * time.Second):
+					Debug.Data["queue"].(comms.RxQueue) <- comms.RxQueueElement{Data: map[string]interface{}{}}
+				}
+			}
+		}()
+	}
+	// On stop
+	Debug.Stop = func() {
+		// Trigger exit
+		Debug.Data["exitTrigger"].(chan struct{}) <- struct{}{}
+		// Wait until channel closed (exit confirmed)
+		<-Debug.Data["exitTrigger"].(chan struct{})
+	}
 }
