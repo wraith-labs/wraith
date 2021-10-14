64ab920b4540fd09fcad229f483202d218297022
0x1a8510f2 <admin@0x1a8510f2.space>
2021-10-14 21:30:39 +0100

    Fix debug part module

--- wraith/stdmod/mod_part/debug.go
+++ wraith/stdmod/mod_part/debug.go
@@ -20,11 +20,14 @@ func (m *DebugModule) ProtoPartModule() {}
 func (m *DebugModule) ProcessProtoPart(hkvs *libwraith.HandlerKeyValueStore, data interface{}) {
 	fmt.Printf("DEBUG: mod_part.DebugModule.ProcessProtoPart called with params: %v | %v\n", hkvs, data)
 
-	dataMap, ok := data.(map[string]interface{})
+	dataString, ok := data.(string)
+
 	if ok {
 		m.w.PushTx(libwraith.TxQueueElement{
-			Addr:     "debug://wraith",
-			Data:     dataMap,
+			Addr: "w.debug://wraith",
+			Data: map[string]interface{}{
+				"DEBUG_DATA": dataString,
+			},
 			Encoding: "w.debug",
 		})
 	}
